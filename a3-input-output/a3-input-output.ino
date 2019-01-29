808091cea0893e49cf799bd837e40a47bf48da0a
Pari G <parijgabriel@gmail.com>
2019-01-29 02:12:47 -0800

    Add final project and assignemnand assignment 3

---
+++ a3-input-output/a3-input-output.ino
@@ -0,0 +1,61 @@
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+*/
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+
+int sensorValue = 0;        // value read from the heat sensor
+int outputValue = 100;       // value output to the PWM (analog out)
+int analogOutPin = 10;      // Analog output pin that the blue LED is attached to
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);   
+  // change the analog out value:
+  analogWrite(analogOutPin, outputValue);
+
+  // if the sensor is heated, change the out pin to power the red LED (pin 11)
+  if (sensorValue < 430) {
+    analogOutPin = 11;
+  }
+  //if sensor is cool, keep the power output going to the blue LED (pin 10)
+  else {
+    analogOutPin = 10;
+  }
+  
+  // set brightness of LED
+  outputValue = 255;
+  // print the results to the Serial Monitor:
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(2);
+}
